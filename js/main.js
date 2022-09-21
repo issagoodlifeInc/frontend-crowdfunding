446b205d95e8e622ef31a385f1a4cfb1dcfba948
Lesley <leskimuti@gmail.com>
2022-09-21 22:28:42 +0300

    Some other changes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
-
-$(document).ready(function(){
-
+$(document).ready(function() {
   // Show pop-up
-  $(".btn-back").click(function(){
-
+  $(".btn-back").click(function() {
     $("#on-pledge").show();
+    $("main").css("visibility", "hidden");
+    $(".brig").css("opacity", "0.8");
   });
   // Close Pop-up
-  $(".exit").click(function(){
+  $(".exit").click(function() {
     $("#on-pledge").hide();
+    $(".brig").css("opacity", "0");
+    $("main").css("visibility", "visible");
+  });
+
+  $(".btn-submit-cash").click(function() {
+    $("#thank-you").show();
+
   });
 
 });
